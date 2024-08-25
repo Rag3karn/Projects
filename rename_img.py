c1016ad9ff4459db27c01c7d00dbe6f99ea8a0ad
Rag3karn <karnguptaprivate123@gmail.com>
2024-08-26 02:29:52 +0530

    Delete rename_img.py

--- rename_img.py
+++
@@ -1,8 +0,0 @@
-import os
-os.chdir('C:\\Users\\karng\\OneDrive\\Desktop\\Object Tracking based on color\\Image Classification Using CNN\\Dataset\\test')
-i = 1
-for file in os.listdir():
-    src = file
-    dst = '0' + '_' + str(i) + '.jpg'
-    os.rename(src,dst)
-    i += 1
